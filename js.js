0797b4625dfe9c3a8d6499fd8aeece3369891834
Evgeniya <zhekanv@icloud.com>
2025-04-29 10:10:56 +0300

    2 изменение

--- js.js
+++ js.js
@@ -3,5 +3,7 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('"Деньги-это побочный продукт того,что вы любите делать" - Стив Джобс')
+	alert('"Деньги-это побочный продукт того,что вы любите делать" - Стив Джобс');
+	image.src = 'money.jpg'; // Замените на URL вашего изображения
+    image.style.display = 'block';
 })
